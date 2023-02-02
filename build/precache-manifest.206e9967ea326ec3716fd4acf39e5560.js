2328ef44f0e52b04705cf37f6266e5faffc2b841
JoseLQB <josequinbla@gmail.com>
2023-02-02 18:17:19 +0100

    fixed error 10 pending fixed deploy

---
+++ build/precache-manifest.206e9967ea326ec3716fd4acf39e5560.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d590ef5a993d336ab70",
+    "url": "/project_react_cv/static/css/main.ffcc8d76.chunk.css"
+  },
+  {
+    "revision": "4d590ef5a993d336ab70",
+    "url": "/project_react_cv/static/js/main.4d590ef5.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/project_react_cv/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/project_react_cv/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "49fc432ae72158ac2147",
+    "url": "/project_react_cv/static/js/runtime~main.49fc432a.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/project_react_cv/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b272be8af4ea8349d0b0c57eea463089",
+    "url": "/project_react_cv/index.html"
+  }
+];
