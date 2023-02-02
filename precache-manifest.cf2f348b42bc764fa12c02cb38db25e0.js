e4de73133f0219bf85255f7cc0659f7eba7ee012
JoseLQB <josequinbla@gmail.com>
2023-02-03 00:41:00 +0100

    Updates

---
+++ precache-manifest.cf2f348b42bc764fa12c02cb38db25e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d590ef5a993d336ab70",
+    "url": "/project_react_cv/build/static/css/main.ffcc8d76.chunk.css"
+  },
+  {
+    "revision": "4d590ef5a993d336ab70",
+    "url": "/project_react_cv/build/static/js/main.4d590ef5.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/project_react_cv/build/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/project_react_cv/build/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "674f4f369270cfa94306",
+    "url": "/project_react_cv/build/static/js/runtime~main.674f4f36.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/project_react_cv/build/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "af49d3278ff8bd5b81297d3c63ac2eac",
+    "url": "/project_react_cv/build/index.html"
+  }
+];
